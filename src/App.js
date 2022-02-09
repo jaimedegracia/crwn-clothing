189e6a98340abb2056d6e9ab03461bdc3f1264be
jaime <jaimedglp@gmail.com>
2022-02-09 01:19:52 +0000

    menu items added without final style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
-import './homepage.component'
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component';
 
 function App() {
   return (
     <div>
-     <HomePage></HomePage>
+     <HomePage/>
     </div>
   );
 }
